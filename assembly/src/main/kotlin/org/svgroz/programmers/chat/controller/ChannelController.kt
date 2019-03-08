65686c012521e9e70d0ef509d67ea482b69c0168
Simon Grozovsky <svgroz@yandex.ru>
2019-03-08 22:09:04 +0300

    add multimodule support

--- assembly/src/main/kotlin/org/svgroz/programmers/chat/controller/ChannelController.kt
+++ assembly/src/main/kotlin/org/svgroz/programmers/chat/controller/ChannelController.kt
@@ -1,5 +1,6 @@
 package org.svgroz.programmers.chat.controller
 
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,9 +12,9 @@ import org.svgroz.programmers.chat.view.ChannelVO
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping(path = ["/api"])
+@RequestMapping(path = ["/api/channel"])
 class ChannelController(private val channelService: ChannelService) {
-    @PostMapping
+    @PostMapping(path = ["/create"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
     fun createChannel(@RequestBody request: CreateChannelRequest): Mono<CreateChannelResponse> {
         return channelService.save(ChannelVO(name = request.name, members = request.members))
                 .map { savedChannel -> CreateChannelResponse(savedChannel.id!!) }
